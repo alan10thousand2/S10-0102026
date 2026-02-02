711c1d186c936551a3989a4398f2ab3fe6cdc468
alan10thousand@hotmail.com <alan10thousand2@hotmail.com>
2026-02-02 18:41:55 +0800

    initial

---
+++ src/main/java/com/example/S10_0102026/S100102026Application.java
@@ -0,0 +1,13 @@
+package com.example.S10_0102026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S100102026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S100102026Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/S10_0102026/S100102026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.S10_0102026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S100102026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
